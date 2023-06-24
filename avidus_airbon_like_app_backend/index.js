8349f654fbdaba1bae0bf1316184f73b8bcf3aaa
Arjun9696 <mallikarjunharalalli@gmail.com>
2023-06-24 18:03:10 +0530

    updated

--- avidus_airbon_like_app_backend/index.js
+++ avidus_airbon_like_app_backend/index.js
@@ -1,24 +1,7 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const { Register, Login } = require('./src/controllers/authController');
-const ListingController = require('./src/controllers/listingController');
-const SearchController = require('./src/controllers/searchController');
-const BookingController = require('./src/controllers/bookingController');
 const connect = require('./src/configs/db');
-app.use(express.json());
-app.use(cors());
-app.post('/register', Register);
-app.post('/login', Login);
-app.use('/listings', ListingController);
-app.use('/search', SearchController);
-app.use('/booking', BookingController);
-app.use('/', (req, res) => {
-    res.json({ message: 'Hello🥳 server is running' })
-});
+const app = require('./servers');
 
-
-const port = process.env.PORT || 9080;
+const port = process.env.PORT || 8080;
 app.listen(port, async () => {
     try {
         await connect();
@@ -27,4 +10,3 @@ app.listen(port, async () => {
         console.log('Err', err);
     }
 });
-module.exports = app;
