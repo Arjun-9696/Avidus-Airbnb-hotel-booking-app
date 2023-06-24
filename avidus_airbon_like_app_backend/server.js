220c2cca77f08ac04e00d224207ef3de41abd03e
Arjun9696 <mallikarjunharalalli@gmail.com>
2023-06-24 17:51:17 +0530

    Update server.js

--- avidus_airbon_like_app_backend/server.js
+++ avidus_airbon_like_app_backend/server.js
@@ -1,7 +1,7 @@
 const connect = require('./src/configs/db');
 const app = require('./index');
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8080;
 app.listen(port, async () => {
     try {
         await connect();
